c16f51dfbc70d740ccd1857013af78c71d874a90
MagnusFFI <mag.aars@gmail.com@>
2015-10-01 16:19:15 +0200

    syncing

--- element.py
+++ element.py
@@ -13,55 +13,55 @@ weights = qn.GLL_weights(N, xis)
 etas = xis
 
 def plotjacobi(gamma1,gamma2,gamma3,gamma4):
-    #Generate mesh:
-    print "Getting Mesh..."
-    X,Y = gh.gordon_hall_grid(gamma1, gamma2, gamma3, gamma4, xis, xis)
+  #Generate mesh:
+	print "Getting Mesh..."
+	X,Y = gh.gordon_hall_grid(gamma1, gamma2, gamma3, gamma4, xis, xis)
 
-    # Get lagrangian derivative matrix:
-    print "Getting derivative matrix..."
-    D = sg.diff_matrix(xis, N)
+	# Get lagrangian derivative matrix:
+	print "Getting derivative matrix..."
+	D = sg.diff_matrix(xis, N)
 
-    # Get Jacobian and total G-matrix:
-    print "Getting Jacobian and G-matrices."
-    Jac, G_tot = lp.assemble_total_G_matrix(np.dot(X,D),
-                                            np.dot(X.T,D),
-                                            np.dot(Y,D),
-                                            np.dot(Y.T,D),
-                                            N,
-                                            N)
-    ##
-    print "Assembling stiffness matrix."
-    A = lp.assemble_local_stiffness_matrix(D, G_tot, N, weights)
-        Jmean = np.mean(Jac)
-        for i in range(N):
-            #print np.max(Jac[N*i:N*(i+1)])-np.min(Jac[N*i:N*(i+1)])
-            print np.max(Jac[N*i:N*(i+1)-1]-Jac[N*i+1:N*(i+1)])
-            #print np.max(Jac[range(i,N*(N-2)+i,N)]-Jac[range(N+i,N*(N-1)+i,N)])
-        #print np.linalg.cond(A)
-        #print G_tot
-        print np.max(Jac)-np.min(Jac)
-        ##Plot all rows:
-        for i in range(N):
-                        plt.plot(X[i,:],Y[i,:],'b')
-                        plt.plot(X[:,i],Y[:,i],'b')
-        fig = plt.figure()
+	# Get Jacobian and total G-matrix:
+	print "Getting Jacobian and G-matrices."
+	Jac, G_tot = lp.assemble_total_G_matrix(np.dot(X,D),
+		np.dot(X.T,D),
+		np.dot(Y,D),
+		np.dot(Y.T,D),
+		N,
+		N)
+	##
+	print "Assembling stiffness matrix."
+	A = lp.assemble_local_stiffness_matrix(D, G_tot, N, weights)
+	Jmean = np.mean(Jac)
+	for i in range(N):
+	  #print np.max(Jac[N*i:N*(i+1)])-np.min(Jac[N*i:N*(i+1)])
+		print np.max(Jac[N*i:N*(i+1)-1]-Jac[N*i+1:N*(i+1)])
+		#print np.max(Jac[range(i,N*(N-2)+i,N)]-Jac[range(N+i,N*(N-1)+i,N)])
+	#print np.linalg.cond(A)
+	#print G_tot
+	print np.max(Jac)-np.min(Jac)
+	##Plot all rows:
+	for i in range(N):
+		plt.plot(X[i,:],Y[i,:],'b')
+		plt.plot(X[:,i],Y[:,i],'b')
+	fig = plt.figure()
 
-        ax = fig.gca(projection='3d')
-        surf = ax.plot_surface(X, Y, (Jac-Jmean).reshape(N,N), rstride=1, cstride=1, cmap=cm.coolwarm,
-                linewidth=0, antialiased=False)
-        plt.show()
+	ax = fig.gca(projection='3d')
+	surf = ax.plot_surface(X, Y, (Jac-Jmean).reshape(N,N), rstride=1, cstride=1, cmap=cm.coolwarm,
+		linewidth=0, antialiased=False)
+	#plt.show()
 
 
-        # CHECKING THE BOUNDARIES
-        x = np.zeros( (N,4) ) #Checking the boundaries
-        y = np.zeros( (N,4) ) #Checking the boundaries
-        x[:,0],y[:,0] = gamma1(xis)
-        x[:,1],y[:,1] = gamma2(xis)
-    x[:,2],y[:,2] = gamma3(xis)
-    x[:,3],y[:,3] = gamma4(xis)
-    plt.plot(x[:,0],y[:,0],'b')
-    plt.plot(x[:,1],y[:,1],'r')
-    plt.plot(x[:,2],y[:,2],'g')
-    plt.plot(x[:,3],y[:,3],'c')
-    plt.show()
-    return
+	# CHECKING THE BOUNDARIES
+	x = np.zeros( (N,4) ) #Checking the boundaries
+	y = np.zeros( (N,4) ) #Checking the boundaries
+	x[:,0],y[:,0] = gamma1(xis)
+	x[:,1],y[:,1] = gamma2(xis)
+	x[:,2],y[:,2] = gamma3(xis)
+	x[:,3],y[:,3] = gamma4(xis)
+	plt.plot(x[:,0],y[:,0],'b')
+	plt.plot(x[:,1],y[:,1],'r')
+	plt.plot(x[:,2],y[:,2],'g')
+	plt.plot(x[:,3],y[:,3],'c')
+	plt.show()
+	return
--- elementcases.py
+++ elementcases.py
@@ -1,38 +1,36 @@
 import numpy as np
 from element import plotjacobi
 
-case = 3
+case = 2
 angle =np.pi/8  # half the size of the total angle 
-c1 = np.cos(np.pi/2-2*angle) # lower Corner x-values
-c2 = np.tan(np.pi/2-2*angle) # Slope for the linear boundaries
+c1 = np.cos(np.pi/2-angle) # lower Corner x-values
+c4 = np.sin(np.pi/2-angle) # lower Corner y-values
+c2 = np.tan(np.pi/2-angle) # Slope for the linear boundaries
 c3 = 0.3 # lateral expansion
 k = 0.7*(1-np.sin(np.pi/2-angle)) # 2nd order coefficient
 
 if case <= 3:
     def gamma1(eta):
-        return c1+c3*(1+eta)/2,c2*c3*(1+eta)/2
-        #return 1,eta # Linear
+        return c1+c3*(1+eta)/2,c4+c2*c3*(1+eta)/2
     if case == 1:
         def gamma2(xi):
-            return -xi*(c1+c3),c2*c3# + k*(1-xi**2)
-            #return xi,1 # Linear
+            theta = -angle*xi+np.pi/2     
+            return (1+c3/c1)*np.cos(theta),c4+c2*c3
     elif case == 2:
-			def gamma2(xi):
-				theta = -angle*xi+np.pi/2     
-				return c3**2*(1+c2**2)*np.cos(theta),c3**2*(1+c2**2)*np.sin(theta)-np.sin(np.pi/2-angle)
-            #return xi,1 # Linear
+        def gamma2(xi):
+            theta = -angle*xi+np.pi/2     
+            return (1+c3/c1)*np.cos(theta),(1+c3/c1)*np.sin(theta)
     elif case == 3:
         def gamma2(xi):
-            return -xi*(c1+c3),c2*c3 + k*(1-xi**2)
-            #return xi,1 # Linear
+            return (c3+c1)*xi,c4+c2*c3+k*(1-xi**2)
     def gamma3(eta):
-        return -c1-c3*(1+eta)/2,c2*c3*(1+eta)/2
+        return -c1-c3*(1+eta)/2,c4+c2*c3*(1+eta)/2
         #return -1,eta # Linear
 
     def gamma4(xi):
         theta = -angle*xi+np.pi/2     
-        return c1*xi, np.sin(theta)-np.sin(np.pi/2-angle)
-        #return np.cos(theta), np.sin(theta)-np.sin(np.pi/2-angle)
+        #return c1*xi, np.sin(theta)-np.sin(np.pi/2-angle)
+        return np.cos(theta), np.sin(theta)
         #return xi,-1 # Linear
 if case == 4 or case == 5:
     def gamma1(eta):
