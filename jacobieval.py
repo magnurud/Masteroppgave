0caac108042b983544874500ce8bcc9dc7df661b
Magnus <mag.aars@gmail.com>
2015-10-04 22:32:56 +0200

    ync

--- jacobieval.py
+++ jacobieval.py
@@ -33,15 +33,16 @@ c = 0.3 # lateral expansion
 d = 1+c # lateral endpoints
 #d = sin(np.pi/2-angle)
 #k = 0.7*(1-np.sin(np.pi/2-angle)) # 2nd order coefficient
+k = 0.3
 theta = pi/4*(2-xi)    
 
 # Defining edge functions 
 ### trapezoidal element with curved bottom
-gamma1 = Matrix([a +c*(1+eta)/2,b*c*(1+eta)/2])
-#gamma2 = Matrix([-xi*(a+c),b*c])# + k*(1-xi**2)
-gamma2 = Matrix([d*cos(theta),d*sin(theta)-sin(pi/4)])
-gamma3 = Matrix([-a-c*(1+eta)/2,b*c*(1+eta)/2])
-gamma4 = Matrix([cos(theta),sin(theta)-sin(pi/4)])
+#gamma1 = Matrix([a +c*(1+eta)/2,b*c*(1+eta)/2])
+##gamma2 = Matrix([-xi*(a+c),b*c])# + k*(1-xi**2)
+#gamma2 = Matrix([d*cos(theta),d*sin(theta)-sin(pi/4)])
+#gamma3 = Matrix([-a-c*(1+eta)/2,b*c*(1+eta)/2])
+#gamma4 = Matrix([cos(theta),sin(theta)-sin(pi/4)])
 #gamma4 = Matrix([a*xi,sqrt(1-(a*xi)**2)])
 ### Circular element 
 #a = 1/np.sqrt(2) # cornervals
@@ -49,13 +50,20 @@ gamma4 = Matrix([cos(theta),sin(theta)-sin(pi/4)])
 #gamma2 = Matrix([-a*xi,sqrt(1-(a*xi)**2)])
 #gamma3 = Matrix([-sqrt(1-(a*eta)**2),-a*eta])
 #gamma4 = Matrix([a*xi,-sqrt(1-(a*xi)**2)])
+### quadratic element with curved bottom (and top
+gamma1 = Matrix([1,eta])
+gamma2 = Matrix([-xi,1 + 0*k/2*(1-xi**2)])
+#gamma2 = Matrix([-xi,1])
+gamma3 = Matrix([-1,-eta])
+gamma4 = Matrix([xi,-1+0*k*(1-xi**2)])
+#gamma4 = Matrix([a*xi,sqrt(1-(a*xi)**2)])
 
 
 #### HAVE TO CHECK THIS !!! 
 # Evaluating in the corners
 expr1 = gamma1; expr2 = gamma2; expr3 = gamma3; expr4 = gamma4;
-c1 = expr4.subs(xi,-1); c2 = expr3.subs(eta,1) # left side corners
-c3 = expr1.subs(eta,-1); c4 = expr2.subs(xi,1) # right side corners
+c1 = expr4.subs(xi,-1); c2 = expr3.subs(eta,-1) # left side corners
+c3 = expr1.subs(eta,-1); c4 = expr2.subs(xi,-1) # right side corners
 # Defining basis functions 
 p = (1-xi)/2; q = (1+xi)/2
 r = (1-eta)/2; s = (1+eta)/2
@@ -74,10 +82,11 @@ Fgh = Fe+Fn-Fen
 J = Matrix([[diff(Fgh[0],xi),diff(Fgh[0],eta)],
     [diff(Fgh[1],xi),diff(Fgh[1],eta)]])
 
+pprint(J)
 # Caldulating determinant and changes in determinant
 Jdet = simplify(J[0,0]*J[1,1]-J[0,1]*J[1,0])
 Jdiv1 = diff(Jdet,xi) 
 #Jdiv2 = diff(Jdet,eta) 
-pprint(collect(Jdet,eta))
-#pprint(J)
+#pprint(collect(Jdet,eta))
+pprint(Jdet)
 
--- nastranmesh/cell.py
+++ nastranmesh/cell.py
@@ -5,6 +5,7 @@ edge2points = {1:[1,2], 2:[2,3], 3:[3,4], 4:[1,4], 5:[5,6], 6:[6,7], 7:[7,8], 8:
 face2points = {1:[1,2,6,5], 2:[2,3,7,6], 3:[4,3,7,8], 4:[1,4,8,5], 5:[1,2,3,4], 6:[5,6,7,8]}
 edge2faces = {1:[1,5], 2:[2,5], 3:[3,5], 4:[4,5], 5:[1,6], 6:[2,6], 7:[3,6], 8:[4,6], 9:[4,1], 10:[1,2], 11:[2,3], 12:[3,4]}
 shadow_face = {1:3, 3:1, 2:4, 4:2, 5:6, 6:5}
+Pts = [] # A list with the position vector of each point
 
 class cell:
     cellID= 0 # Id number
@@ -19,18 +20,48 @@ class cell:
     Faces = []  # A list of Faces
     infliction_faces= [] # A list of lists, containing direction of infliction corresponding to each curved edge max 4 directions
     ### LOCAL MAPPING FACES ###
-    face_glloc = {}
-    face_locgl = {}
-    points_glloc = {}
+    face_glloc = {} # Global to local face mapping
+    face_locgl = {} # Local to global face mapping
+    points_glloc = {} #
     ### LOCAL MAPPINGS WITHIN AN ELEMENT DEFINED AS A GLOBAL DICT ### 
 
     n = 2 # The number of cells to inflict
 #----------- Class functions -----------#
 #----------- --------------- -----------#
+    def print_info():
+        ''' Prints the information of a cell'''
+        print ('cell nr. {}'.format(self.cellID))
+        print ('list of vertices:\n')
+        for i in range(21):
+            print self.vertices[i]
+        print 'Curved edges: \n'
+        if not curved_edge: print 'No registered curved edges'
+        else: print curved_edge
+        print 'Neighbours, corresponding face and global ID: \n'
+        for n in range(6):
+            print n , nbours[n]
+        print ' neighbours where curvature will be propagated: \n'
+        for n in infliction_faces:
+            print n
+        print 'That was all the information \n'
+        print '-------------||----------------'
+        
+    def calc_midpoint(info_array,edge):
+        '''Calculates the relative position of the given midpoint on edge 
+        k is the relative position on edge, and h is the offset from edge.'''
+        global Pts
+        x1,x2 = self.vertices[edge2points(edge)]
+        x3 = info_array[3:6] 
+        a = Pts[x2]-Pts[x1]
+        b = Pts[x3]-Pts[x1]
+        k = np.dot(a,a)/np.dot(a,b)*sqrt(np.dot(a,a)) # rel distance in kji direction from corner x1. 
+        h = np.dot(b-k*a,b-k*a) # rel distance in eta direction from the line x2-x1
+        return k,h/2
+
     def define_face_mappings():
-        'A function that defines the mappings from global to local faces both ways'
+        '''A function that defines the mappings from global to local faces both ways'''
         count = 0
-        for globface in Faces:
+        for globface in self.Faces:
             count = count+1
             self.face_glloc[globface.faceID] = count
             self.face_locgl[count] = globface.faceID
@@ -39,7 +70,7 @@ class cell:
             print ('only %d registered faces' % count)
 
     def opornot(edge,f): 
-        'determining whether an edge belongs to face or the opposite one'
+        '''determining whether an edge belongs to face or the opposite one'''
         count = 0
         for node in edge2points[edge]:
             for node2 in face2points[f]:
@@ -50,7 +81,7 @@ class cell:
         else: print('in element {}, face {} and edge {} has {} common pts'.format(self.cellID,f,edge,count))
 
     def create_info():
-        'creating the curvature info to be passed on to the next cell'
+        '''creating the curvature info to be passed on to the next cell'''
         if curved_edge == {}: 
             print 'no curved info to be created'
             return 
@@ -61,10 +92,11 @@ class cell:
             for f in infliction_faces[edge]: # iterating over the faces corresponding to edge
                 opposite = opornot(edge,f) 
                 pts = self.vertices[edge2points[edge]] #global points
-                info_nbour.append([f,opposite,pts,curved_edge_info[edge],self.n-1])
+                midpoint = calc_midpoint(curved_edge_info[edge],edge)
+                info_nbour.append([f,opposite,pts,midpoint,self.n-1])
 
     def invoke_midpoint(information):
-        'adding a curvature given the input'
+        '''adding a curvature given the input'''
         # Find the corresponding edge 
             # Find the corresponding face
         face = face_glloc[globface] # The local face
@@ -79,7 +111,7 @@ class cell:
         self.curved_edge_info = transforminfo(information)
 
     def inflict_midpoint():
-        'inflicting the neighbours with curved edge'
+        '''inflicting the neighbours with curved edge'''
         for ne in nbours:
             nbours_list[ne].invoke_midpoint(info_nbour[ne])
             nbours_list[ne].update_midpoint()
